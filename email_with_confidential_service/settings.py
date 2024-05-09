2fd7318368ab56f6d3cd07bbe574e289d8d002de
gtmetric <ly.soksedtha81@gmail.com>
2024-05-09 11:33:14 +0800

    Add secret key placeholder

--- email_with_confidential_service/settings.py
+++ email_with_confidential_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tj(i^3&sq6#m21zqk%65j!@5-j8v7n5p%$^#k7-nho)ns2we7^'
+SECRET_KEY = '<add-your-secret-key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
